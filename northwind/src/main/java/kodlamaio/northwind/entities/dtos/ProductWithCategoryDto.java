cd6fff9cc874c7ade9d9b236e7c4f4146b3ffb1e
Ömer Faruk Çiçek <omarfaruk_cicek@outlook.com>
2021-05-31 00:32:44 +0300

    31.05.2021

---
+++ northwind/src/main/java/kodlamaio/northwind/entities/dtos/ProductWithCategoryDto.java
@@ -0,0 +1,14 @@
+package kodlamaio.northwind.entities.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProductWithCategoryDto {
+	private int id;
+	private String categoryName;
+	private String productName;
+	
+	
+}
